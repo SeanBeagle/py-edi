a062939181ec53b9cd02718f76f6ef9970ed85fa
Sean Beagle <seanbeagle@Seans-MacBook-Pro.local>
2021-06-10 18:12:50 -0400

    Created base classes for TransactionSet, Segment, DataElement. Created EDIParser utility class

---
+++ information_unit.py
@@ -0,0 +1,114 @@
+class DataElement:
+    _string = str()
+
+    def __init__(self, string):
+        self._string = string
+
+    def __str__(self):
+        return self._string
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self})'
+
+    @property
+    def string(self):
+        return self._string
+
+
+class Segment:
+    _line = str()
+    _data_element_separator = str()
+    _data_elements = list()
+
+    def __init__(self, line, data_element_separator='*'):
+        self._line = line
+        self._data_element_separator = data_element_separator
+        self._data_elements = EdiParser.get_data_elements(line, data_element_separator)
+
+    def __str__(self):
+        return self._line
+
+    def __repr__(self):
+        return {f'{self.__class__.__name__}({self})'}
+
+    @property
+    def line(self):
+        return self._line
+
+    @property
+    def data_elements(self):
+        return self._data_elements
+
+    @property
+    def data_element_separator(self):
+        return self._data_element_separator
+
+
+class TransactionSet:
+    _segments = list()
+    _segment_terminator = str()
+
+    def __init__(self, file):
+        self._segment_terminator = EdiParser.get_segment_terminator(file)
+        self._segments = EdiParser.get_segments(file, self.segment_terminator)
+
+    def __str__(self):
+        return "Transaction Set"
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self[:10]}...)'
+
+    def __len__(self):
+        return len(self.segments)
+
+    @property
+    def segments(self):
+        return self._segments
+
+    @property
+    def segment_terminator(self):
+        return self._segment_terminator
+
+
+class EdiParser:
+    @staticmethod
+    def get_segment_terminator(file):
+        terminator = None
+        with open(file, 'r') as fh:
+            char = fh.read(1)
+            while char:
+                if char == '>':
+                    terminator = fh.read(1)
+                elif terminator and char == 'G':
+                    return terminator
+                char = fh.read(1)
+
+    @staticmethod
+    def get_element_separator(file):
+        with open(file, 'r') as fh:
+            string = fh.read(4)
+            if string.startswith('ISA'):
+                return string[3]
+            else:
+                raise Exception('File does not start with ISA')
+
+    @staticmethod
+    def get_segments(file, segment_terminator):
+        segments = []
+        with open(file, 'r') as fh:
+            segment = ''
+            char = fh.read(1)
+            while char:
+                if char == segment_terminator:
+                    segments.append(Segment(segment))
+                    segment = ''
+                else:
+                    segment += char
+                char = fh.read(1)
+                if not char:
+                    segments += segment
+        return segments
+
+    @staticmethod
+    def get_data_elements(line, data_element_separator):
+        return [DataElement(item) for item in line.split(data_element_separator)]
--- main.py
+++ main.py
@@ -1,60 +1,15 @@
-from edi_parser import EdiParser
-from data_element import data_element
-from transaction_set import EDI850
-import unittest
+from information_unit import TransactionSet
 
 if __name__ == '__main__':
-    '''
-    file = "/Users/seanbeagle/go/ElectronicDataInterchange/data/810-OUT_884_20210201_100923.DMS"
-    parser = EdiParser()
-    parser.parse(file)
+    file = "tests/test_data/EDI-810.txt"
+    ts = TransactionSet(file)
 
-    for element in data_element.DataElement.all():
-        print(element)
-    '''
-
-    class Dog:
-        name = str()
-        _id = int()
-        breed = str()
-        color = str()
-
-        def __init__(self, name, id, breed, color):
-            self.name = name.capitalize()
-            self.id = id
-            self.breed = breed
-            self.color = color
-
-        @property
-        def id(self):
-            return self._id
-
-        @id.setter
-        def id(self, id):
-            if id < 1:
-                self._id = id
-                raise Exception("ID out of range")
-            else:
-                print("dats a goooood id")
-                self._id = id
-
-        def bark(self):
-            print(self.name + " the " + self.breed + " says WOOF!")
+    print(f'The Transaction Set contains {len(ts)} segments')
+    for segment in ts.segments:
+        print(segment)
 
 
-    test_dog = Dog("rufus", 1, 'poodle', 'white')
 
-    class TestDogClass(unittest.TestCase):
-        def test_id_less_than_one_throws_exception(self):
-            self.assertRaises(Exception, Dog, 'rufus', 0, 'poodle', 'white')
-
-        def test_breed_is_poodle(self):
-            self.assertEqual(test_dog.breed, 'shepherd')
-
-
-    tc = TestDogClass()
-    tc.test_id_less_than_one_throws_exception()
-    tc.test_breed_is_poodle()
 
 
 
